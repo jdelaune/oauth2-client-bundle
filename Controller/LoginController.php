dc4b620ef911b314e5a80cfe61b58268725c47fb
Jordan de Laune <jordan.delaune@gmail.com>
2013-09-30 15:26:51 +0100

    Add default login path

---
+++ Controller/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OAuth2\ClientBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="_logout")
+     * @Method({"GET"})
+     */
+    public function loginAction()
+    {
+        // Do Nothing
+    }
+}
