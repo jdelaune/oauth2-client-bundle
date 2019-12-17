6db0a2a78d8f77802efebdc1582cce49e8870357
Jordan de Laune <jordan@chargifi.com>
2019-12-17 11:59:51 +0000

    Update for Symfony 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,12 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('o_auth2_client');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $treeBuilder = new TreeBuilder('oauth2_client');
 
         return $treeBuilder;
     }
